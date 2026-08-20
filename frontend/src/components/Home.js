ee52c21a77df2fc34e658548e37f1eb9ff321c13
jenny.jione.jang <jenny.jione.jang@gmail.com>
2026-08-20 22:33:13 +0900

    feat: 입력에 따른 실시간 단어 필터링 추가 - 글자를 입력할 때마다 매칭되는 결과만 테이블에 남도록 함 - ESC를 누르면 input이 비활성화되는 기능도 있음

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -6,6 +6,8 @@ import { API_URL } from "../constants";
 
 function Home() {
   const [words, setWords] = useState([]);
+  const [search, setSearch] = useState("");
+
 
   const fetchAllWords = async () => {
     const res = await fetch(`${API_URL}/words_list`);
@@ -17,14 +19,45 @@ function Home() {
     fetchAllWords();
   }, []);
 
+  const filteredWords = words.filter((word) =>
+    word.word.includes(search) ||
+    word.hiragana.includes(search) ||
+    word.meaning.includes(search) ||
+    word.korean.includes(search)
+  );
+
   return (
     <div style={{ padding: "20px" }}>
       <div class="section-header">
         <h1 className="section-title">전체 단어 목록</h1>
+        <div className="search-box">
+          <span className="search-icon">⌕</span>
+
+          <input
+            type="text"
+            placeholder="단어 검색..."
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Escape") {
+                e.target.blur();
+              }
+            }}
+          />
+
+          {search && (
+            <button onClick={() => setSearch("")}>×</button>
+          )}
+        </div>
         <div className="detail">(total: {words.length})</div>
       </div>
+
       {/* ✅ 갱신 함수도 props로 전달 */}
-      <WordTable words={words} refreshWords={fetchAllWords} />
+      <WordTable
+        words={filteredWords}
+        refreshWords={fetchAllWords}
+        />
+      {/* <WordTable words={words} refreshWords={fetchAllWords} /> */}
     </div>
   );
 }
